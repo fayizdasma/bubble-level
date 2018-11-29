e1199a4abc57729cd46f38ee7f80c65c3284dd39
fayis.musthafa <fayis.musthafa@student.griffith.ie>
2018-11-29 14:35:26 +0000

    fixed 2D bubble to be in the range [-10,10] reduced code duplication

--- app/src/main/java/com/fm/bubblelevel/view/custom/BubbleLevel.java
+++ app/src/main/java/com/fm/bubblelevel/view/custom/BubbleLevel.java
@@ -16,10 +16,20 @@ public class BubbleLevel extends View {
     private static final float BUBBLE_RADIUS = 55f;
     private static final float MAX_RANGE = 10;
     private static final float MIN_RANGE = -10;
+
+    private static final int TYPE_PORTRAIT = 1;
+    private static final int TYPE_LANDSCAPE = 2;
+    private static final int TYPE_FLAT = 3;
+
     private Paint paint;
     private String TAG = "lvl";
     private SensorData sensorData;
     private int screenOrientation;
+    private int centerWidth;
+    private int centerHeight;
+    private int viewWidth;
+    private int viewHeight;
+    private int outerBorderWidth = 100;
 
 
     public BubbleLevel(Context context, @Nullable AttributeSet attrs) {
@@ -35,72 +45,129 @@ public class BubbleLevel extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.d(TAG, "onDraw: width: " + getWidth() + " height: " + getHeight());
-        if (sensorData != null) {
+        viewWidth = getWidth();
+        viewHeight = getHeight();
+        centerWidth = viewWidth / 2;
+        centerHeight = viewHeight / 2;
 
+        Log.d(TAG, "onDraw: width: " + viewWidth + " height: " + viewHeight);
+        if (sensorData != null) {
             //if device portrait or landscape, draw 1D bubble; else draw 2D bubble
             if ((screenOrientation == 0 | screenOrientation == 180) && ((sensorData.getRoll() >= 45 && sensorData.getRoll() < 135) || (sensorData.getRoll() <= -45 && sensorData.getRoll() > -135))) {
                 Log.d(TAG, "type: 1D Bubble portrait");
-                paint.setStyle(Paint.Style.FILL);
-
-                if (sensorData.getPitch() > MAX_RANGE) {
-                    paint.setColor(Color.RED);
-                    canvas.drawCircle((float) (getWidth() - (MAX_RANGE * 10 + getWidth() / 2)), (float) getHeight() / 2, BUBBLE_RADIUS, paint);
-                } else if (sensorData.getPitch() < MIN_RANGE) {
-                    paint.setColor(Color.RED);
-                    canvas.drawCircle((float) (getWidth() - (MIN_RANGE * 10 + getWidth() / 2)), (float) getHeight() / 2, BUBBLE_RADIUS, paint);
-                } else {
-                    paint.setColor(Color.GREEN);
-                    canvas.drawCircle((float) (getWidth() - (sensorData.getPitch() * 10 + getWidth() / 2)), (float) getHeight() / 2, BUBBLE_RADIUS, paint);
-                }
-                //draw center dot
-                paint.setColor(Color.BLACK);
-                canvas.drawCircle(getWidth() / 2, getHeight() / 2, 5f, paint);
-
-                //outer border circle
-                paint.setStyle(Paint.Style.STROKE);
-                canvas.drawRect(0, (getHeight() / 2) - 100, getWidth(), (getHeight() / 2) + 100, paint);
+                draw1DBubble(canvas, TYPE_PORTRAIT);
                 invalidate();
-
             } else if (screenOrientation == 90 | screenOrientation == 270) {
                 Log.d(TAG, "type: 1D Bubble landscape");
-                paint.setStyle(Paint.Style.FILL);
-                paint.setColor(Color.RED);
-
-                if (sensorData.getRoll() > MAX_RANGE) {
-                    paint.setColor(Color.RED);
-                    canvas.drawCircle((float) getWidth() / 2, (float) (getHeight() - (MAX_RANGE * 10 + getHeight() / 2)), BUBBLE_RADIUS, paint);
-                } else if (sensorData.getRoll() < MIN_RANGE) {
-                    paint.setColor(Color.RED);
-                    canvas.drawCircle((float) getWidth() / 2, (float) (getHeight() - (MIN_RANGE * 10 + getHeight() / 2)), BUBBLE_RADIUS, paint);
-                } else {
-                    paint.setColor(Color.GREEN);
-                    canvas.drawCircle((float) getWidth() / 2, (float) (getHeight() - (sensorData.getRoll() * 10 + getHeight() / 2)), BUBBLE_RADIUS, paint);
-                }
-                //draw center dot
-                paint.setColor(Color.BLACK);
-                canvas.drawCircle(getWidth() / 2, getHeight() / 2, 5f, paint);
-
-                //outer border circle
-                paint.setStyle(Paint.Style.STROKE);
-                canvas.drawRect((getWidth() / 2) - 100, 0, (getWidth() / 2) + 100, getHeight(), paint);
+                draw1DBubble(canvas, TYPE_LANDSCAPE);
                 invalidate();
             } else {
                 Log.d(TAG, "type: 2D Bubble flat");
-                paint.setStyle(Paint.Style.FILL);
-                paint.setColor(Color.RED);
-                canvas.drawCircle((float) sensorData.getPitch() * 10 + getWidth() / 2, (float) (getHeight() - (sensorData.getRoll() * 10 + getHeight() / 2)), BUBBLE_RADIUS, paint);
-
-                //draw center dot
-                paint.setColor(Color.BLACK);
-                canvas.drawCircle(getWidth() / 2, getHeight() / 2, 5f, paint);
-
-                //outer border circle
-                paint.setStyle(Paint.Style.STROKE);
-                canvas.drawCircle(getWidth() / 2, getHeight() / 2, ((float) 10 + getHeight() / 2), paint);
+                draw2DBubble(canvas);
                 invalidate();
             }
         }
+    }
+
+    private void draw2DBubble(Canvas canvas) {
+        float cx = 0;
+        float cy = 0;
+        paint.setStyle(Paint.Style.FILL);
+
+        //check max/min range
+        if (sensorData.getRoll() > MAX_RANGE || sensorData.getPitch() > MAX_RANGE) {
+            paint.setColor(Color.RED);
+            if (sensorData.getRoll() > MAX_RANGE) {
+                cx = (float) (sensorData.getPitch() * 10 + centerWidth);
+                cy = (float) (viewHeight - (MAX_RANGE * 10 + centerHeight));
+            } else if (sensorData.getPitch() > MAX_RANGE) {
+                cx = (float) (MAX_RANGE * 10 + centerWidth);
+                cy = (float) (viewHeight - (sensorData.getRoll() * 10 + centerHeight));
+            }
+        } else if (sensorData.getRoll() < MIN_RANGE || sensorData.getPitch() < MIN_RANGE) {
+            paint.setColor(Color.RED);
+            if (sensorData.getRoll() < MIN_RANGE) {
+                cx = (float) (sensorData.getPitch() * 10 + centerWidth);
+                cy = (float) (viewHeight - (MIN_RANGE * 10 + centerHeight));
+            } else if (sensorData.getPitch() < MIN_RANGE) {
+                cx = (float) (MIN_RANGE * 10 + centerWidth);
+                cy = (float) (viewHeight - (sensorData.getRoll() * 10 + centerHeight));
+            }
+        } else {
+            paint.setColor(Color.GREEN);
+            cx = (float) sensorData.getPitch() * 10 + centerWidth;
+            cy = (float) (viewHeight - (sensorData.getRoll() * 10 + centerHeight));
+        }
+        canvas.drawCircle(cx, cy, BUBBLE_RADIUS, paint);
+
+        //draw center dot
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(centerWidth, centerHeight, 5f, paint);
+
+        //outer border circle
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(centerWidth, centerHeight, ((float) 10 + centerHeight), paint);
+    }
+
+    private void draw1DBubble(Canvas canvas, int type) {
+        float cx;
+        float cy;
+        int borderLeft;
+        int borderTop;
+        int borderRight;
+        int borderBottom;
+        paint.setStyle(Paint.Style.FILL);
+
+        //check max/min range
+        if (type == TYPE_PORTRAIT) {
+            if (sensorData.getPitch() > MAX_RANGE) {
+                paint.setColor(Color.RED);
+                cx = viewWidth - (MAX_RANGE * 10 + centerWidth);
+                cy = centerHeight;
+            } else if (sensorData.getPitch() < MIN_RANGE) {
+                paint.setColor(Color.RED);
+                cx = viewWidth - (MIN_RANGE * 10 + centerWidth);
+                cy = centerHeight;
+            } else {
+                paint.setColor(Color.GREEN);
+                cx = (float) (viewWidth - (sensorData.getPitch() * 10 + centerWidth));
+                cy = centerHeight;
+            }
+            //outer border params
+            borderLeft = 0;
+            borderTop = (centerHeight) - outerBorderWidth;
+            borderRight = viewWidth;
+            borderBottom = (centerHeight) + outerBorderWidth;
+        } else {
+            if (sensorData.getRoll() > MAX_RANGE) {
+                paint.setColor(Color.RED);
+                cx = centerWidth;
+                cy = (viewHeight - (MAX_RANGE * 10 + centerHeight));
+            } else if (sensorData.getRoll() < MIN_RANGE) {
+                paint.setColor(Color.RED);
+                cx = centerWidth;
+                cy = (viewHeight - (MIN_RANGE * 10 + centerHeight));
+            } else {
+                paint.setColor(Color.GREEN);
+                cx = centerWidth;
+                cy = (float) (viewHeight - (sensorData.getRoll() * 10 + centerHeight));
+            }
+
+            //outer border params
+            borderLeft = (centerWidth) - outerBorderWidth;
+            borderTop = 0;
+            borderRight = (centerWidth) + outerBorderWidth;
+            borderBottom = viewHeight;
+        }
+        canvas.drawCircle(cx, cy, BUBBLE_RADIUS, paint);
+
+        //draw center dot
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(centerWidth, centerHeight, 5f, paint);
+
+        //outer border
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(borderLeft, borderTop, borderRight, borderBottom, paint);
     }
 
     //method to call the draw function
